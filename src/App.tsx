1e05d78c9418babd8e441658709b222c12af00d6
Can Soylu <hsyncn17@gmail.com>
2021-09-24 20:51:52 +0300

    created slice

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,34 @@
+import { useState } from "react";
+import todoSlice, { add } from "./features/todoSlice";
+import { useAppDispatch, useAppSelector } from "./store";
+
 function App() {
-  return <div className="App"></div>;
+  const todos = useAppSelector((state) => state.todos);
+  const [title, setTitle] = useState("");
+
+  const dispatch = useAppDispatch();
+
+  const onSave = () => {
+    dispatch(add(title));
+    setTitle("");
+  };
+
+  return (
+    <div className="App">
+      <input
+        name="title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        type="text"
+      />
+      <button onClick={onSave}>Save</button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.title}>{todo.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
